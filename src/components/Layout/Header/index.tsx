402819013aa175637d6f05103531080abe31fadb
bdnetworks <saakib.com@gmail.com>
2025-07-27 14:15:35 +0600

    Update index.tsx

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -213,7 +213,19 @@ const Header: React.FC = () => {
           </nav>
         </div>
       </div>
-    </header>
+  <!-- Icon Font Stylesheet -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="https://themewagon.github.io/elearning/lib/animate/animate.min.css" rel="stylesheet">
+    <link href="https://themewagon.github.io/elearning/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="https://themewagon.github.io/elearning/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Template Stylesheet -->
+    <link href="https://themewagon.github.io/elearning/css/style.css" rel="stylesheet">  </header>
   );
 };
 
